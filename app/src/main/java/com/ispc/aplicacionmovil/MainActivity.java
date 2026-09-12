83d362747a1454fc777c8b778faeededbf8c3359
gastón_Alonso() <tonga88@live.com.ar>
2026-09-12 12:16:05 -0300

    UI: implementar pantallas iniciales (Splash, Login y Main) con navegación

--- app/src/main/java/com/ispc/aplicacionmovil/MainActivity.java
+++ app/src/main/java/com/ispc/aplicacionmovil/MainActivity.java
@@ -3,12 +3,24 @@ package com.ispc.aplicacionmovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvBienvenida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvBienvenida = findViewById(R.id.tvBienvenida);
+
+        // Recuperar el dato enviado desde LoginActivity
+        if (getIntent() != null && getIntent().hasExtra("EXTRA_USUARIO")) {
+            String nombreUsuario = getIntent().getStringExtra("EXTRA_USUARIO");
+            tvBienvenida.setText("¡Bienvenido/a, " + nombreUsuario + "!");
+        }
     }
 }
