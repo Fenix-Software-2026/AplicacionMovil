540b3bfa12cac3c1274979a9e36aab02bcc53bf0
JDR89 <joaquin.romero89@gmail.com>
2026-09-11 21:26:55 -0300

    Implemento Splash, Login y navegacion entre Activities

---
+++ app/src/main/java/com/ispc/aplicacionmovil/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.ispc.aplicacionmovil;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Temporizador para cambiar de pantalla automáticamente
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // Crear el Intent explícito hacia LoginActivity
+                Intent intent = new Intent(
+                        SplashActivity.this,
+                        LoginActivity.class
+                );
+
+                startActivity(intent);
+
+                // Cerrar SplashActivity para que el usuario
+                // no vuelva a ella con el botón Atrás
+                finish();
+            }
+        }, 2500); // Tiempo en milisegundos
+    }
+}
